efbbb2bc20adb6b42e33385e8f0727da03e5cdc3
AlekseiQwert007 <ap1429773@gmail.com>
2024-08-15 12:31:30 +0400

    Add files via upload

---
+++ Nachalo.py
@@ -0,0 +1,10 @@
+#1ST programm
+print(9**0.5*5)
+#2nd programm
+print(9.99>9.98, 1000!=1000.1)
+#3rd programm
+print(2*2+2, 2*(2+2))
+print(2*2+2 == 2*(2+2))
+#4th programm
+a=(int(float('123.456')*10))%10
+print('первая после запятой - ', a)
